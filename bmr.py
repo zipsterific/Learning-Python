9475889014c87bc8513b3473323a809e7e27df1b
Al <fatbikeguy@protonmail.com>
2019-02-14 20:07:24 -0600

    Comment : updates on Thu Feb 14 20:07:24 CST 2019

--- bmr.py
+++ bmr.py
@@ -3,7 +3,15 @@
 weight = int(input("Enter your weight in kg:\n")) # convert user input into an int
 height = int(input("Enter your height in cm:\n"))
 age = int(input("Enter your age:\n"))
-isMale = bool(input("Are you male? (y/n)"))
+isMale = str(input("Are you male? (y/n)"))
+
+if isMale == "y":
+    isMale = True
+elif isMale == "n":
+    isMale = False
+else:
+    print("Error")
+    quit()
 
 if isMale:
     bmr = 66.5 + (13.75 * weight) + (5 * height) - (6.755 * age)
